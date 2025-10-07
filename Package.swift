65683f373328403df7a840200ca152f8664a5d89
Jürgen Schuck <otabuzzman@users.noreply.github.com>
2025-10-07 21:29:58 +0200

    Update version

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
             targets: ["AppModule"],
             bundleIdentifier: "com.otabuzzman.sdk85.ios",
             teamIdentifier: "28FV44657B",
-            displayVersion: "1.6.8",
-            bundleVersion: "62",
+            displayVersion: "1.6.9",
+            bundleVersion: "63",
             appIcon: .asset("AppIcon"),
             accentColor: .presetColor(.teal),
             supportedDeviceFamilies: [
